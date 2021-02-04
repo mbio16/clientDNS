dc7afcd8ca5961883cd8249cfd6d8f2bd9a38925
Martin Biolek <43006943+mbio16@users.noreply.github.com>
2021-02-04 19:21:42 +0100

    DS in progress

---
+++ src/main/java/enums/DIGEST_TYPE.java
@@ -0,0 +1,21 @@
+package enums;
+
+
+public enum DIGEST_TYPE {
+	RESERVED((byte) 0x00),
+	SHA1((byte) 0x01),
+	SHA256((byte) 0x02);
+	
+	private byte code;
+	
+	private DIGEST_TYPE(byte code) {
+		this.code = code;
+	}
+	
+    public static DIGEST_TYPE getTypeByCode(byte code){
+        for(DIGEST_TYPE type : DIGEST_TYPE.values()){
+            if(type.code == code) return type;
+        }
+        return null;
+    }
+}
